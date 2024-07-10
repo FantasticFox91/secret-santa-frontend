e8e2e481eeb2fa7d36ecffd08ece166268022e5c
Grygorii Lysytsia <lisitsa-gp@ranepa.ru>
2024-07-10 16:28:01 +0300

    dashboard-page: add dashboard page with users filter by status

---
+++ components/DashboardListWithFilters/DashboardListWithFilters.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="dashboard-list-with-filters">
+    <FiltersList @statusChanged="onStatusChange"/>
+    <InvitationList dashboard :statusFilter="statusFilter"/>
+  </div>
+</template>
+
+<script setup lang='ts'>
+import './dashboard-list-with-filters.scss';
+
+const statusFilter = ref('');
+
+const onStatusChange = (value) => {
+  statusFilter.value = value;
+}
+</script>
---
+++ components/FiltersList/FiltersList.vue
@@ -0,0 +1,59 @@
+<template>
+    <ul class="filters-list">
+      <li class="filters-list__item" :class="{'filters-list__item--transparent': statusFilter !== 'declined' && statusFilter !== ''}">
+        <FormFieldDashboard v-model="statusFilter" :id="'declined'" :number="declinedUsers" @onChange="onFilterChange" @resetFilter="onResetStatusFilter">
+          declined
+        </FormFieldDashboard>
+      </li>
+      <li class="filters-list__item form-field-dashboard--pending" :class="{'filters-list__item--transparent': statusFilter !== 'pending' && statusFilter !== ''}">
+        <FormFieldDashboard v-model="statusFilter" :id="'pending'" :number="pendingUsers" @onChange="onFilterChange" @resetFilter="onResetStatusFilter">
+          pending
+        </FormFieldDashboard>
+      </li>
+      <li class="filters-list__item" :class="{'filters-list__item--transparent': statusFilter !== 'accepted' && statusFilter !== ''}">
+        <FormFieldDashboard v-model="statusFilter" :id="'accepted'" :number="acceptedUsers" @onChange="onFilterChange" @resetFilter="onResetStatusFilter">
+          accepted
+        </FormFieldDashboard>
+      </li>
+    </ul>
+</template>
+
+<script setup lang='ts'>
+import './filters-list.scss';
+
+const emits = defineEmits(['statusChanged'])
+const statusFilter = ref('');
+const userStore = useUserStore();
+
+const users = computed(() => {
+  return userStore?.userEvent?.userStatus || [];
+});
+const userStatusCounts = computed(() => {
+  return users.value.reduce(
+    (counts: {declined: number, accepted: number, pending: number}, user: {status: 'DECLINED' | 'ACCEPTED' | 'PENDING'}) => {
+      if (user.status === 'DECLINED') {
+        counts.declined++;
+      } else if (user.status === 'ACCEPTED') {
+        counts.accepted++;
+      } else if (user.status === 'PENDING') {
+        counts.pending++;
+      }
+      return counts;
+    },
+    { declined: 0, accepted: 0, pending: 0 }
+  );
+});
+
+const declinedUsers = computed(() => userStatusCounts.value.declined);
+const acceptedUsers = computed(() => userStatusCounts.value.accepted);
+const pendingUsers = computed(() => userStatusCounts.value.pending);
+
+const onFilterChange = (value) => {
+  statusFilter.value = value;
+  emits('statusChanged', value);
+}
+
+const onResetStatusFilter = () => {
+  statusFilter.value = '';
+}
+</script>
